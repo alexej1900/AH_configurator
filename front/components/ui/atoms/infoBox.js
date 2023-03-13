f4816e71f9bf0f090f319db9eaa8ac97495061f8
aliaksei.badnarchuk <aliaksei.badnarchuk@calydo.com>
2023-03-13 15:57:45 +0100

    fix infobox style

--- front/components/ui/atoms/infoBox.js
+++ front/components/ui/atoms/infoBox.js
@@ -8,16 +8,18 @@ export default function InfoBox({styleTitle, description}) {
   const [colapsed, setCollapsed] = useState(true);
 
   return (
-    <div className={[styles.infoBox, !colapsed ? styles.open : styles.closed].join(' ')}>
-      <div className={styles.text_block}>
-        <div className={styles.text_block__title}> {styleTitle} </div>
-        <div className={styles.close} onClick={() => setCollapsed(!colapsed)}>
-          <IconComponent name={colapsed ? 'info' : 'close'} color="#000"/>
+    description 
+      ? <div className={[styles.infoBox, !colapsed ? styles.open : styles.closed].join(' ')}>
+        <div className={styles.text_block}>
+          <div className={styles.text_block__title}> {styleTitle} </div>
+          <div className={styles.close} onClick={() => setCollapsed(!colapsed)}>
+            <IconComponent name={colapsed ? 'info' : 'close'} color="#000"/>
+          </div>
+        </div>
+        <div className={styles.text_block__text}>
+          <p dangerouslySetInnerHTML={{ __html: description }}></p>
         </div>
       </div>
-      <div className={styles.text_block__text}>
-        <p dangerouslySetInnerHTML={{ __html: description }}></p>
-      </div>
-    </div>
+    : null
   )
 }

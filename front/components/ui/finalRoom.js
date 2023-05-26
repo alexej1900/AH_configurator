eb62be1ab73e3c64cfe0cb3f6f61f9294cbb36e9
aliaksei.badnarchuk <aliaksei.badnarchuk@calydo.com>
2023-05-26 08:43:00 +0200

    fix image size in big screens

--- front/components/ui/finalRoom.js
+++ front/components/ui/finalRoom.js
@@ -189,8 +189,8 @@ export default function FinalRoom({ roomName, style }) {
             id={roomName + 'img'} 
             src={roomImage.url} 
             layout={"fixed"} 
-            width={window.innerWidth * window.devicePixelRatio} // avoid problems with pdf rendering & retina screens
-            height={window.innerWidth / 2 * window.devicePixelRatio} 
+            width={window.innerWidth < 1500 ? window.innerWidth * window.devicePixelRatio : 1500 * window.devicePixelRatio} // avoid problems with pdf rendering & retina screens
+            height={window.innerWidth < 1500 ? window.innerWidth / 2 * window.devicePixelRatio : 750 * window.devicePixelRatio} 
             priority="true" 
             alt="Room Image"
           />}
